5966dc3738bbbdbb7bbe553d5190cda53a8692fe
frgnca <fg@frgn.ca>
2017-05-09 17:55:20 -0400

    Fix header

--- SOURCE/AudioDeviceCmdlets.cs
+++ SOURCE/AudioDeviceCmdlets.cs
@@ -1,6 +1,6 @@
 /*
+Copyright (c) 2016-2017 Francois Gendron <fg@frgn.ca>
 The MIT License (MIT)
-Copyright (c) 2016-2017 Francois Gendron fg@frgn.ca
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
