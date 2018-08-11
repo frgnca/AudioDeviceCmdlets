aace4d3b04b95b93e6b73f8aee3205c4e0cc03bc
frgnca <fg@frgn.ca>
2018-08-11 12:16:33 -0400

    change version number

--- SOURCE/Properties/AssemblyInfo.cs
+++ SOURCE/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
